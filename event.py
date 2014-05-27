6d6b59fae37da551ca4eb19893cfcb28de474194
Aubort Jean-Baptiste <jean-baptiste.aubort@epfl.ch>
2014-05-27 17:33:40 +0200

    send func

--- event.py
+++ event.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python2
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('form.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+    services = []
+    if request.method == 'POST':
+        services.append(send_techup())
+    return render_template('send.html', services=services)
+
+def send_techup():
+    return {'name': 'techup', 'url': 'http://google.com'}
 
 if __name__ == '__main__':
     app.debug = True
