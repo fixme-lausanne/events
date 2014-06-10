12a07cd57c2f9d8ec864e1e361c3dbd76ad05395
Aubort Jean-Baptiste <jean-baptiste.aubort@epfl.ch>
2014-06-10 13:20:36 +0200

    Add GPLv3 statment in every source files

--- event.py
+++ event.py
@@ -2,6 +2,23 @@
 # -*- coding: utf8 -*-
 #from __future__ import unicode_literals
 
+'''
+This file is part of FIXME Events.
+
+FIXME Events is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FIXME Events is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FIXME Events. If not, see <http://www.gnu.org/licenses/>.
+'''
+
 from flask import Flask, render_template, request, url_for, redirect, session
 import random, sys
 from services import *
